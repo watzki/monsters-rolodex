2fa9f0a491e9424d312e3cdacb3f7df643433be9
Edwardo Ramirez Datu <Edward_R_Datu@manulife.com>
2019-09-11 08:22:13 +0800

    Updates

---
+++ precache-manifest.4a3270b71b6fcf118bb8e02ca1ce9af1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116f624e411ca4b497aa07fc298333ca",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6ec271315d2c327f85b9",
+    "url": "/monsters-rolodex/static/css/main.3558772b.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "6ec271315d2c327f85b9",
+    "url": "/monsters-rolodex/static/js/main.195d415b.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
